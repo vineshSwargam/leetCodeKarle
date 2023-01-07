f8becb1d30ee2df3adff6ade4d69d1606cf7b34a
Vinesh Swargam <43958050+vineshSwargam@users.noreply.github.com>
2023-01-08 00:40:27 +0530

    Create twoSum.java

---
+++ twoSum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+      return findTwoSum(nums, target);
+  }
+
+  public static int[] findTwoSum(int arr[], int target) {
+    int size = arr.length;
+    Hashtable<Integer, Integer> elemIndexMap = new Hashtable<Integer, Integer>();
+    for(int i=0; i<size; i++){
+      int secVal = target - arr[i];
+      if (elemIndexMap.containsKey(secVal)) {
+        return new int[] {elemIndexMap.get(secVal), i};
+      } else {
+        elemIndexMap.put(arr[i], i);
+      }
+    }
+    return new int[] {};
+  }
+}
