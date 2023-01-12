3316e4f25db91acb67b992578fb4278468da8c86
Vinesh Swargam <43958050+vineshSwargam@users.noreply.github.com>
2023-01-12 22:55:48 +0530

    Create removeDuplicates.java

---
+++ removeDuplicates.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/solutions/?orderBy=most_votes
+
+class Solution {
+  public int removeDuplicates(int[] nums) {
+    int k = 1;
+    for(int i=1; i<nums.length; i++){
+      if(nums[i]!=nums[i-1]){
+        nums[k] = nums[i];
+        k++;
+      }
+    }
+    return k;
+  }
+}
